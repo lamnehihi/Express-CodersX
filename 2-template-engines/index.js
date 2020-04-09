02c35f1277c9465089b4f4515f4e562987695da1
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-04-09 19:56:15 +0700

    3-query-parameters

--- 2-template-engines/index.js
+++ 2-template-engines/index.js
@@ -3,6 +3,11 @@ var app = express();
 
 var port = 3000;
 
+var users = [
+  {id : 1, name : 'Thanh lam'},
+  {id : 2, name : 'Ngoc Phuc'},
+];
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 
@@ -15,13 +20,20 @@ app.get('/',function(req, res) {
 
 app.get('/users',function(req, res) {
   res.render('users/index', {
-    users : [
-      {id : 1, name : 'Thanh lam'},
-      {id : 2, name : 'Ngoc Phuc'},
-    ]
+    users : users
   });
 });
 
+app.get('/users/search', function(req, res) {
+  var q = req.query.q;
+  var searchUser = users.filter(function(user) {
+    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  });
+  res.render('users/index', {
+    users : searchUser,
+    q
+  });
+});
 
 app.listen(port, function() {
   console.log('Example app listening at http://localhost' + port);
