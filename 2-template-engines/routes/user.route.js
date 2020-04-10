3fa50b00686716367a366365721a4d77fffb07d8
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-04-10 21:14:26 +0700

    9-MVC-model

--- 2-template-engines/routes/user.route.js
+++ 2-template-engines/routes/user.route.js
@@ -1,43 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var controller = require('../controllers/user.controller');
 
-var db = require('../db');
-var shortid = require('shortid');
+router.get('/', controller.index);
 
-router.get('/',function(req, res) {
-  res.render('users/index', {
-    users : db.get('users').value()
-  });
-});
+router.get('/search', controller.search);
 
-router.get('/search', function(req, res) {
-  var q = req.query.q;
-  var searchUser = db.get('users').value().filter(function(user) {
-    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
-  res.render('users/index', {
-    users : searchUser,
-    q
-  });
-});
+router.get('/create', controller.create);
 
-router.get('/create', function(req, res) {
-  res.render('users/create', {
-  });
-});
+router.get('/:userId', controller.findUser);
 
-router.get('/:userId', function (req, res) {
-  var id = req.params.userId;
-  var user = db.get('users').find({ id: id}).value();
-  res.render('users/view', {
-    user : user
-  });
-});
-
-router.post('/create', function (req, res) {
-  req.body.id = shortid.generate();
-  db.get('users').push(req.body).write();
-  res.redirect('/users');
-});
+router.post('/create', );
 
 module.exports = router;
