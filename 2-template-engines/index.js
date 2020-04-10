72d2ba11d32dd53efe4477ee4426ea73a99c4954
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-04-10 19:31:43 +0700

    4-post-method

--- 2-template-engines/index.js
+++ 2-template-engines/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
+var low = require('lowdb')
+var FileSync = require('lowdb/adapters/FileSync')
+ 
+var adapter = new FileSync('db.json')
+var db = low(adapter)
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Set some defaults
+db.defaults({ users: {} })
+  .write()
 
 var port = 3000;
 
-var users = [
-  {id : 1, name : 'Thanh lam'},
-  {id : 2, name : 'Ngoc Phuc'},
-];
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -20,19 +28,29 @@ app.get('/',function(req, res) {
 
 app.get('/users',function(req, res) {
   res.render('users/index', {
-    users : users
+    users : db.get('users').value()
   });
 });
 
 app.get('/users/search', function(req, res) {
   var q = req.query.q;
-  var searchUser = users.filter(function(user) {
+  var searchUser = db.get('users').value().filter(function(user) {
     return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   });
   res.render('users/index', {
     users : searchUser,
     q
   });
+});
+
+app.get('/users/create', function(req, res) {
+  res.render('users/create', {
+  });
+});
+
+app.post('/users/create', function (req, res) {
+  db.get('users').push(req.body).write();
+  res.redirect('/users');
 });
 
 app.listen(port, function() {
